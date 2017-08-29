6c5bca13f5e14feab5871a5ad7378296b4822ad1
Ronald Haring <ronald@otly.net>
2017-08-29 16:31:31 +0200

    using local storage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -7,14 +7,24 @@ class App extends React.Component {
         super();
         this.selectCity = this.selectCity.bind(this);
     }
+   
     selectCity(city) {
         // event.preventDefault()
         if(city.id && city.href) {
             console.log("selectedcity id: " + city.id);
         }
+        localStorage.setItem('selectedCity', JSON.stringify(city));
+        this.setState({selectedCity : city})
         // fetch the movies
         //and store these in the state
         // then show the movies page
+    }
+
+    componentWillMount(){
+        let item = localStorage.getItem('selectedCity');
+        if (item){
+            this.setState({selectedCity : JSON.parse(item)})
+        }
     }
     render() {
         return (
